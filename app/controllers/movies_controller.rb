13913b1f45e35699f37d1c9d53b9de8357172897
Sean Regmund <sregmund719@gmail.com>
2018-09-24 03:31:14 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,13 @@
+#when reopening, use these commmands:
+#rake db:drop
+#rake db:create
+#rake db:migrate
+#rake db:seed
+
+#QUESTION FOR COMPLETEION...PART 1 
+#are my id: title_header and id: release_date_header under index.html.haml correct
+#for the "IMPORTANT for grading purposes: Secion"
+
 class MoviesController < ApplicationController
 
   def movie_params
@@ -10,8 +20,29 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+#def index
+    #@movies = Movie.all
+#end
+#case params[:sort]
+#     when 'title'
+#        @movies = Movie.order("title asc")
+#        @title_hilite = 'hilite'
+#    when 'release'
+#        @movies = Movie.order("release_date asc")
+#        @release_hilite = 'hilite'
+#    else
+        
   def index
-    @movies = Movie.all
+    case params[:sort]
+    when 'title'
+        @movies = Movie.order(params[:sort])
+        @movie_highlight = 'hilite'     #created a class in movies/index.html.haml to use this, 'hilite from default.css'
+    when 'release_date'
+        @movies = Movie.order(params[:sort])    
+        @release_highlight = 'hilite'   #created a class in movies/index.html.haml to use this, 'hilite from default.css'
+    else
+        @movies = Movie.all
+    end
   end
 
   def new
